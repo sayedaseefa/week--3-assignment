8d1e84432b49881bcd1a9a8203b3f2148d8956be
sayedaseefa <81669636+sayedaseefa@users.noreply.github.com>
2021-04-25 11:28:14 +0530

    Add files via upload

---
+++ WDemo.java
@@ -0,0 +1,18 @@
+class WDemo
+{
+public static void main(String a[])
+{
+int J=10;
+boolean b1=true;
+Integer iobj=J;
+Boolean bobj=b1;
+System.out.println(iobj);
+System.out.println(bobj);
+int K=iobj;
+boolean b=bobj;
+System.out.println(K);
+System.out.println(b);
+}
+}
+
+
